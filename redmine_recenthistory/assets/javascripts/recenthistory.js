f4c4ae411853fea63837ba6ff3652f9a4072678b
Kota Shiratsuka <kota@insaneworks.co.jp>
2013-08-05 04:28:06 +0900

    Compatible with Redmine 2.x
    * prototype.js -> jQuery Conversions.
    * adds new jquery.cookie.js

--- redmine_recenthistory/assets/javascripts/recenthistory.js
+++ redmine_recenthistory/assets/javascripts/recenthistory.js
@@ -1,196 +1,86 @@
 /* Copyright 2011 Dustin Lambert - Licensed under: GNU Public License 2.1 or later */
+/* https://github.com/lmbrt/Redmine-Recent-Issue-List/blob/master/redmine_recenthistory/assets/javascripts/recenthistory.js */
+/* Modified by Kota Shiratsuka [Compatible with Redmine 2.x] */
+/* https://github.com/kotashiratsuka/Redmine-Recent-Issue-List */
 
 var issueMatcher = new RegExp(/.*\/issues\/(\d+).*/gi);
 var currentIssue = null;
-var maxIssues = 10;
+var maxIssues = 15;
 
-function trackRecentHistory()
-{
-	
-	var results = issueMatcher.exec(location.href);
-	if (results == undefined || results == null || results.length < 2 || results[1].length < 1)
-	{
-		return;
+Object.size = function(obj) {
+	var size = 0, key;
+	for (key in obj) {
+		if (obj.hasOwnProperty(key)) size++;
 	}
+	return size;
+};
+
+function trackRecentHistory() {
+
+	var results = issueMatcher.exec(location.href);
+	if (results == undefined || results == null || results.length < 2 || results[1].length < 1) { return; }
+
 	currentIssue = results[1];
 	var issueTitle = null;
 	var issueSubject = null;
-	
-	$$("#content h2:first").each(function(e)
-	{
-		issueTitle = e.innerHTML.replace(/^.*#(\d+).*$/, "#$1");
-	});
 
-	$$("#content div.subject h3").each(function(e)
-	{
-		issueSubject = e.innerHTML;
-	});
-		
-	if (issueTitle === null || issueSubject === null)
-	{
-		return;
-	}
+	var issueTitle = $("#content h2:first").text().replace(/.*(#\d+)/,"$1");
+	var issueSubject = $("#content div.subject h3").text()
+	var issueArray = $.cookie("recentIssues")
 
-	var recentIssues = new JsonCookies();
-	issueArray = recentIssues.get("recentIssues");
-	if (! issueArray)
-	{
-		issueArray = [];
-	}
+	if (! issueArray) { var issueArray = []; }
 
 	var issueHash = {};
-	
 	issueHash[currentIssue]=1;
-	var newArray = [];
-	var cidx = 1;	
-	for (var i = 0; i < issueArray.length; i++)
-	{
-		if (i <= maxIssues)
-		{
+	var newArray = []
+	var cidx = 1;
+
+	for (var i = 0; i < Object.size(issueArray); i++) {
+		if (i <= maxIssues) {
 			var entry = issueArray[i];
-			if (issueHash[entry.ID] != 1)
-			{
+			if (issueHash[entry.ID] != 1) {
 				issueHash[entry.ID] = 1;
 				newArray[cidx]=entry;
 				cidx++;
 			}
 		}
 	}
-	
 	issueArray = newArray;
-	
-		
-	var thisEntry = {
-		ID: currentIssue,
-		Str: issueTitle + ": " + issueSubject
-	};
-	
-	issueArray[0] = thisEntry;
-	recentIssues.set("recentIssues", issueArray, 45);
-	
 
+	var thisEntry = {
+			ID: currentIssue,
+			Str: issueTitle + ": " + issueSubject
+	};
+	issueArray[0] = thisEntry;
+
+	$.cookie("recentIssues", issueArray, {path: '/'})
 }
 
 function showRecentHistory()
 {
-	if($("sidebar"))
-	{
-		var recentIssues = new JsonCookies();
-	        issueArray = recentIssues.get("recentIssues");
+	if($("sidebar")) {
+		var issueArray = $.cookie("recentIssues");
 
+		$("<div id='recentList'>").appendTo("#sidebar");
+		var recentList = $("<h3>最近見たチケット</h3>").appendTo("#recentList");
 
-		var recentList = new Element("h3");
-		recentList.update("最近表示したチケット");
-
-	        
-		$("sidebar").insert({top:recentList});
 		var issuesShown = 0;
-		for (var i = issueArray.length - 1; i >= 0; i--)
-		{
-			var entry = issueArray[i];
-
-			if (entry.ID != currentIssue)
-			{
-				issuesShown++;
-				var entryitem = new Element("a");
-				entryitem.setAttribute("href", "/issues/" + entry.ID);
-				var disp = entry.Str;
-				if (disp.length > 55)
-				{
-					disp = disp.substring(0, 55) + "..."
-				}
-				entryitem.update(disp + "<br>" );
-				recentList.insert({after:entryitem});
+		for (i = 0; i < Object.size(issueArray); i++) {
+			if (issueArray[i]["ID"] != currentIssue) {
+			issuesShown++;
+			var a = "<a href=/issues/" + issueArray[i]["ID"] + ">" + issueArray[i]["Str"] + "</a></br>"
+			$("#recentList").append(a);
 			}
 		}
-		
-		if (issuesShown < 1)
-		{
-			recentList.hide();
+
+		if (issuesShown < 1) {
+			$("#recentList").hide()
 		}
 	}
-
 }
 
-document.observe('dom:loaded', function(){
+$(document).ready(function() {
+	$.cookie.json = true;
 	trackRecentHistory();
 	showRecentHistory();
-});
-
-var Cookies = Class.create({
-    initialize: function(path, domain) {
-        this.path = path || '/';
-        this.domain = domain || null;
-    },
-    // Sets a cookie
-    set: function(key, value, days) {
-        if (typeof key != 'string') {
-            throw "Invalid key";
-        }
-        if (typeof value != 'string' && typeof value != 'number') {
-            throw "Invalid value";
-        }
-        if (days && typeof days != 'number') {
-            throw "Invalid expiration time";
-        }
-        var setValue = key+'='+escape(new String(value));
-        if (days) {
-            var date = new Date();
-            date.setTime(date.getTime()+(days*24*60*60*1000));
-            var setExpiration = "; expires="+date.toGMTString();
-        } else var setExpiration = "";
-        var setPath = '; path='+escape(this.path);
-        var setDomain = (this.domain) ? '; domain='+escape(this.domain) : '';
-        var cookieString = setValue+setExpiration+setPath+setDomain;
-        document.cookie = cookieString;
-    },
-    // Returns a cookie value or false
-    get: function(key) {
-        var keyEquals = key+"=";
-        var value = false;
-        document.cookie.split(';').invoke('strip').each(function(s){
-            if (s.startsWith(keyEquals)) {
-                value = unescape(s.substring(keyEquals.length, s.length));
-                throw $break;
-            }
-        });
-        return value;
-    },
-    // Clears a cookie
-    clear: function(key) {
-        this.set(key,'',-1);
-    },
-    // Clears all cookies
-    clearAll: function() {
-        document.cookie.split(';').collect(function(s){
-            return s.split('=').first().strip();
-        }).each(function(key){
-            this.clear(key);
-        }.bind(this));
-    }
-});
-
-var JsonCookies = Class.create(Cookies, {});
-JsonCookies.addMethods({
-    // Overridden set method to JSON-encode value
-    set: function($super, key, value, days) {
-        switch (typeof value) {
-            case 'undefined':
-            case 'function':
-            case 'unknown':
-                throw "Invalid value type";
-                break;
-            case 'boolean':
-            case 'string':
-            case 'number':
-                value = String(value.toString());
-            break;
-        }
-        $super(key, Object.toJSON(value), days);
-    },
-    // Overriden get method to JSON-decode the value
-    get: function($super, key) {
-        var value = $super(key);
-        return (value) ? value.evalJSON() : false;
-    }
 });
--- redmine_recenthistory/init.rb
+++ redmine_recenthistory/init.rb
@@ -4,14 +4,14 @@ Redmine::Plugin.register :redmine_recenthistory do
   name 'Redmine Recent History'
   author 'Dustin Lambert'
   description 'This is a Redmine plugin which will keep up with each user\'s history and show a recent issue list'
-  version '0.0.1'
+  version '0.0.2'
 end
 
 class RecentHistoryViewListener < Redmine::Hook::ViewListener
 
   # Adds javascript and stylesheet tags
   def view_layouts_base_html_head(context)
-    javascript_include_tag('recenthistory', :plugin => :redmine_recenthistory) 
+    javascript_include_tag('recenthistory', 'jquery.cookie.js', :plugin => :redmine_recenthistory)
   end
 
 end
